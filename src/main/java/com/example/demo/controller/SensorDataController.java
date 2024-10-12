da8ecd87268b10a1a9217b26be7f51e7550e1971
zangizntPT <giangPT@apero.vn>
2024-10-12 16:57:43 +0700

    server runable

--- src/main/java/com/example/demo/controller/SensorDataController.java
+++ src/main/java/com/example/demo/controller/SensorDataController.java
@@ -4,8 +4,6 @@ import com.example.demo.model.SensorData;
 import com.example.demo.service.SensorDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
